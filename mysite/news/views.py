c1f5ef47fe03806d083e759b1ed628c846c2a91d
Nikolay <mikkikyz@mail.ru>
2022-04-17 20:37:50 +0300

    Lesson 12 Django 18.04 Вывод на странице Списка новостей

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from .models import News
+from .models import News
+
 
 def index(request):
-    print(request)
-    return HttpResponse('H')
-#     news = News.objects.order_by('-created_at')
-#     context = {'news': news, 'title': 'Список новостей'}
-#     return render(request, 'news/index.html', context)
+    news = News.objects.order_by('-created_at')
+    context = {'news': news, 'title': 'Список новостей'}
+    return render(request, 'news/index.html', context)
