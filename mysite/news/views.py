1cadc9f6c341ca535334a370404cdcdd7f7d9359
Nikolay <mikkikyz@mail.ru>
2022-05-04 17:15:20 +0300

    Start Lesson 43 Django 22.04 +Миксины

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import News, Category
 from .forms import NewsForm
+from .utils import MyMixin
 
 
-class HomeNews(ListView):
+class HomeNews(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    # queryset = News.objects.select_related('category')
-    # extra_context = {'title': 'Главная'}
+    mixin_prop = 'hello word'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
+        context['title'] = self.get_upper('Главная страница')
+        context['mixin_prop'] = self.get_prop()
         return context
 
     def get_queryset(self):
         return News.objects.filter(is_published=True).select_related('category')
 
 
-class NewsByCategory(ListView):
+class NewsByCategory(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
@@ -30,24 +32,27 @@ class NewsByCategory(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        context['title'] = self.get_upper(Category.objects.get(pk=self.kwargs['category_id']))
         return context
 
     def get_queryset(self):
-        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True) \
+            .select_related('category')
+
 
 class ViewNews(DetailView):
     model = News
-    context_object_name ='news_item'
+    context_object_name = 'news_item'
     # template_name = 'news/news_detail.html'
     # pk_url_kwarg = 'news_id'
 
 
-class CreateNews(CreateView):
+class CreateNews(LoginRequiredMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
     # success_url = reverse_lazy('home')
-
+    # login_url = '/admin/'
+    raise_exception = True
 
 # def index(request):
 #     news = News.objects.order_by('-created_at')
